5faaccd9519f585fd3e4ecf6ab2801518167521f
Lovett Li <lovett.li@liferay.com>
2016-02-05 11:42:01 +0800

    [migrator] The aui:column taglib has been removed and replaced with aui:col taglib

---
+++ com.liferay.blade.test/src/com/liferay/blade/test/AUIColumnTagsTest.java
@@ -0,0 +1,49 @@
+package com.liferay.blade.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import com.liferay.blade.api.Migration;
+import com.liferay.blade.api.Problem;
+import com.liferay.blade.util.NullProgressMonitor;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Test;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+
+public class AUIColumnTagsTest {
+
+	@Test
+	public void findProblems() throws Exception {
+		ServiceReference<Migration> sr = context
+				.getServiceReference(Migration.class);
+
+		Migration m = context.getService(sr);
+
+		List<Problem> problems = m.findProblems(new File("jsptests/aui-column/"), new NullProgressMonitor());
+
+		assertEquals(1, problems.size());
+
+		boolean found = false;
+
+		for (Problem problem : problems) {
+			if (problem.file.getName().endsWith("AUIColumnTagTest.jsp")) {
+				if (problem.lineNumber == 2 && problem.startOffset == 58 && problem.endOffset == 461) {
+					found = true;
+				}
+			}
+		}
+
+		if (!found) {
+			fail();
+		}
+	}
+
+	private final BundleContext context = FrameworkUtil.getBundle(
+		this.getClass()).getBundleContext();
+
+}
