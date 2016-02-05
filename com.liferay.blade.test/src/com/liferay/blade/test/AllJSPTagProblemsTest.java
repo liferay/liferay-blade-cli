dd04c765cbbbdf3b2da99508119044c4fa2e4a93
Lovett Li <lovett.li@liferay.com>
2016-02-05 11:45:51 +0800

    [migrator] update total error number of jsp tags

--- com.liferay.blade.test/src/com/liferay/blade/test/AllJSPTagProblemsTest.java
+++ com.liferay.blade.test/src/com/liferay/blade/test/AllJSPTagProblemsTest.java
@@ -23,7 +23,7 @@ public class AllJSPTagProblemsTest {
 		Migration m = context.getService(sr);
 		List<Problem> problems = m.findProblems(new File("jsptests/"), new NullProgressMonitor());
 
-		final int expectedSize = 56;
+		final int expectedSize = 57;
 		final int size = problems.size();
 
 		if (size != expectedSize) {
