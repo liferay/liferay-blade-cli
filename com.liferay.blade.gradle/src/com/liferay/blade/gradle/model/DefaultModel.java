ac694e55be24a904540d007d2987bd12e2f7d7b5
Gregory Amerson <gregory.amerson@liferay.com>
2016-02-11 13:11:01 -0600

    Add new project com.liferay.blade.gradle to hold model and plugin bundles

---
+++ com.liferay.blade.gradle/src/com/liferay/blade/gradle/model/DefaultModel.java
@@ -0,0 +1,32 @@
+package com.liferay.blade.gradle.model;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.Set;
+
+/**
+ * @author Gregory Amerson
+ */
+@SuppressWarnings("serial")
+public class DefaultModel implements Serializable {
+
+    private final Set<String> _pluginClassNames;
+    private final Set<File> _outputFiles;
+
+    public DefaultModel(Set<String> pluginClassNames, Set<File> outputFiles) {
+        _pluginClassNames = pluginClassNames;
+        _outputFiles = outputFiles;
+    }
+
+    public boolean hasPlugin(String pluginClassName) {
+        return _pluginClassNames.contains(pluginClassName);
+    }
+
+    public Set<File> getOutputFiles() {
+        return _outputFiles;
+    }
+
+    public Set<String> getPluginClassNames() {
+        return _pluginClassNames;
+    }
+}
