dff7e63272b251e36a4647862c2f915f762908d6
Gregory Amerson <gregory.amerson@liferay.com>
2016-02-11 13:27:26 -0600

    New GradleTooling class for getting tooling model

---
+++ com.liferay.blade.cli/src/com/liferay/blade/cli/gradle/GradleTooling.java
@@ -0,0 +1,116 @@
+package com.liferay.blade.cli.gradle;
+
+import aQute.bnd.osgi.Jar;
+import aQute.bnd.osgi.Processor;
+import aQute.bnd.osgi.Resource;
+import aQute.lib.io.IO;
+
+import com.liferay.blade.gradle.model.CustomModel;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.gradle.tooling.GradleConnector;
+import org.gradle.tooling.ModelBuilder;
+import org.gradle.tooling.ProjectConnection;
+
+/**
+ * @author Gregory Amerson
+ */
+public class GradleTooling {
+
+	private static void copy(InputStream in, File outputDir) throws Exception {
+
+		try (Jar jar = new Jar("dot", in)) {
+			for (Entry<String, Resource> e : jar.getResources().entrySet()) {
+				String path = e.getKey();
+
+				Resource r = e.getValue();
+
+//				path = path.replaceAll(type + "/" + template + "/", "");
+
+				File dest = Processor.getFile(outputDir, path);
+
+				if ((dest.lastModified() < r.lastModified()) ||
+					(r.lastModified() <= 0)) {
+
+					File dp = dest.getParentFile();
+
+					if (!dp.exists() && !dp.mkdirs()) {
+						throw new Exception("Could not create directory " + dp);
+					}
+
+					IO.copy(r.openInputStream(), dest);
+				}
+			}
+		}
+	}
+
+	private static <T> T getModel(
+		Class<T> modelClass, File cacheDir, File projectDir ) throws Exception {
+
+		T retval = null;
+
+		final GradleConnector connector = GradleConnector.newConnector();
+		connector.forProjectDirectory(projectDir);
+
+		ProjectConnection connection = null;
+
+		try {
+			connection = connector.connect();
+			ModelBuilder<T> modelBuilder =
+				(ModelBuilder<T>) connection.model(modelClass);
+
+			final File depsDir = new File(cacheDir, "deps");
+
+			if (!depsDir.exists()) {
+				depsDir.mkdirs();
+
+				InputStream in =
+					GradleTooling.class.getResourceAsStream("/deps.zip");
+
+				copy(in, depsDir);
+			}
+
+			final String initScriptTemplate =
+				IO.collect(
+					GradleTooling.class.getResourceAsStream("init.gradle"));
+
+			String path = depsDir.getAbsolutePath();
+
+			path = path.replaceAll("\\\\", "/");
+
+			final String initScriptContents =
+				initScriptTemplate.replaceFirst("%deps%", path);
+
+			File scriptFile = Files.createTempFile(
+				"blade", "init.gradle").toFile();
+
+			IO.write(initScriptContents.getBytes(), scriptFile);
+
+			modelBuilder.withArguments("--init-script", scriptFile.getAbsolutePath());
+
+			retval = modelBuilder.get();
+		}
+		finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+
+		return retval;
+	}
+
+	public static Set<File> getOutputFiles(File cacheDir, File buildDir)
+		throws Exception {
+
+		final CustomModel model =
+			getModel(CustomModel.class, cacheDir, buildDir);
+
+		return model.getOutputFiles();
+	}
+
+}
