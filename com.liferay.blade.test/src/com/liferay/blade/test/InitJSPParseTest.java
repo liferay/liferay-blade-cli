2ec988bf7aed3d7d68c0b2413bc79c93a33f5cfa
Andy Wu <andy.wu@liferay.com>
2016-06-23 14:14:24 +0800

    add moved classes imports checker with auto correct context

--- com.liferay.blade.test/src/com/liferay/blade/test/InitJSPParseTest.java
+++ com.liferay.blade.test/src/com/liferay/blade/test/InitJSPParseTest.java
@@ -42,7 +42,7 @@ public class InitJSPParseTest {
 
 		List<Problem> problems = m.findProblems(new File("jsptests/jukebox-portlet/"), new NullProgressMonitor());
 
-		assertEquals(31, problems.size());
+		assertEquals(34, problems.size());
 
 		boolean found = false;
 
