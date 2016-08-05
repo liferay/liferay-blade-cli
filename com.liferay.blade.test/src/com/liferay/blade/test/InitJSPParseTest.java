448f82921c2a8763354fbeb81a2d65a96adbf49f
Lovett Li <lovett.li@liferay.com>
2016-08-05 09:55:07 +0800

    [migrator] update total number of problem

--- com.liferay.blade.test/src/com/liferay/blade/test/InitJSPParseTest.java
+++ com.liferay.blade.test/src/com/liferay/blade/test/InitJSPParseTest.java
@@ -42,7 +42,7 @@ public class InitJSPParseTest {
 
 		List<Problem> problems = m.findProblems(new File("jsptests/jukebox-portlet/"), new NullProgressMonitor());
 
-		assertEquals(34, problems.size());
+		assertEquals(388, problems.size());
 
 		boolean found = false;
 
