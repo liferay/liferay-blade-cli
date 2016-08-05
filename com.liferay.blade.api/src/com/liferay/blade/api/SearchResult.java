2525e640f03779142172e0aaa6a58010ef4717fd
Gregory Amerson <gregory.amerson@liferay.com>
2016-08-05 09:45:25 -0500

    improved equals and hashcode by using generated equals/hashCode from Eclipse

--- com.liferay.blade.api/src/com/liferay/blade/api/SearchResult.java
+++ com.liferay.blade.api/src/com/liferay/blade/api/SearchResult.java
@@ -41,21 +41,48 @@ public class SearchResult{
 
 	@Override
 	public boolean equals(Object obj) {
-		if (obj instanceof SearchResult) {
-			SearchResult sr = (SearchResult) obj;
-
-			if (startLine == sr.startLine && startOffset == sr.startOffset 
-					&& endOffset == sr.endOffset) {
-				return true;
-			}
-		}
-
-		return false;
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SearchResult other = (SearchResult) obj;
+		if (autoCorrectContext == null) {
+			if (other.autoCorrectContext != null)
+				return false;
+		} else if (!autoCorrectContext.equals(other.autoCorrectContext))
+			return false;
+		if (endLine != other.endLine)
+			return false;
+		if (endOffset != other.endOffset)
+			return false;
+		if (file == null) {
+			if (other.file != null)
+				return false;
+		} else if (!file.equals(other.file))
+			return false;
+		if (fullMatch != other.fullMatch)
+			return false;
+		if (startLine != other.startLine)
+			return false;
+		if (startOffset != other.startOffset)
+			return false;
+		return true;
 	}
 
 	@Override
 	public int hashCode() {
-		return file.hashCode();
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((autoCorrectContext == null) ? 0 : autoCorrectContext.hashCode());
+		result = prime * result + endLine;
+		result = prime * result + endOffset;
+		result = prime * result + ((file == null) ? 0 : file.hashCode());
+		result = prime * result + (fullMatch ? 1231 : 1237);
+		result = prime * result + startLine;
+		result = prime * result + startOffset;
+		return result;
 	}
 
 }
