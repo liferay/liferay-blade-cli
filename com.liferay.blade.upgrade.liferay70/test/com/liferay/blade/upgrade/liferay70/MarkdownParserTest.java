67a439aecc2eb873f51919fdd26ef55618f4d472
Joye Luo <joye.luo@liferay.com>
2016-11-04 15:44:27 +0800

    update BREAKING_CHANGES.markdown file and test

--- com.liferay.blade.upgrade.liferay70/test/com/liferay/blade/upgrade/liferay70/MarkdownParserTest.java
+++ com.liferay.blade.upgrade.liferay70/test/com/liferay/blade/upgrade/liferay70/MarkdownParserTest.java
@@ -30,6 +30,6 @@ public class MarkdownParserTest {
 		Map<String, String> tags = MarkdownParser.parse("BREAKING_CHANGES.markdown");
 
 		assertNotNull(tags);
-		assertEquals(112, tags.size());
+		assertEquals(114, tags.size());
 	}
 }
