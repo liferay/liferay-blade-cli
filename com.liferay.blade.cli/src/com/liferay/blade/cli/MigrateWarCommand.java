3e0532b55c6d571af7d5b8e9a86e22482c574438
Terry Jia <terry.jia@liferay.com>
2016-11-29 11:06:01 +0800

    add migrateServiceBuilder Command

--- com.liferay.blade.cli/src/com/liferay/blade/cli/MigrateWarCommand.java
+++ com.liferay.blade.cli/src/com/liferay/blade/cli/MigrateWarCommand.java
@@ -27,7 +27,6 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.nio.file.CopyOption;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -259,36 +258,6 @@ public class MigrateWarCommand {
 		}
 
 		IO.delete(themePlugin);
-	}
-
-	private static class CopyDirVisitor extends SimpleFileVisitor<Path> {
-	    private final Path fromPath;
-	    private final Path toPath;
-	    private final CopyOption copyOption;
-
-	    public CopyDirVisitor(Path fromPath, Path toPath, CopyOption copyOption) {
-	        this.fromPath = fromPath;
-	        this.toPath = toPath;
-	        this.copyOption = copyOption;
-	    }
-
-	    @Override
-	    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-	        Path targetPath = toPath.resolve(fromPath.relativize(dir));
-
-	        if( !Files.exists(targetPath) ) {
-	            Files.createDirectory(targetPath);
-	        }
-
-	        return FileVisitResult.CONTINUE;
-	    }
-
-	    @Override
-	    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-	        Files.copy(file, toPath.resolve(fromPath.relativize(file)), copyOption);
-
-	        return FileVisitResult.CONTINUE;
-	    }
 	}
 
 	private void migrateLayoutPlugin(File layoutPlugin) throws Exception {
