3ab2cdcba78d2d0d497dc3b1916d67664d8c1e89
Gregory Amerson <gregory.amerson@liferay.com>
2017-09-28 16:31:53 -0500

    add tests for properties locator

---
+++ com.liferay.blade.cli/src/com/liferay/properties/locator/PropertiesLocatorArgs.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.properties.locator;
+
+import com.beust.jcommander.Parameter;
+
+import java.io.File;
+
+/**
+ * @author Gregory Amerson
+ */
+public class PropertiesLocatorArgs {
+
+	public PropertiesLocatorArgs() {
+	}
+
+	public File getBundleDir() {
+		return _bundleDir;
+	}
+
+	public File getOutputFile() {
+		return _outputFile;
+	}
+
+	public File getPropertiesFile() {
+		return _propertiesFile;
+	}
+
+	public boolean isHelp() {
+		return _help;
+	}
+
+	public boolean isQuiet() {
+		return _quiet;
+	}
+
+	public void setBundleDir(File bundleDir) {
+		_bundleDir = bundleDir;
+	}
+
+	public void setOutputFile(File outputFile) {
+		_outputFile = outputFile;
+	}
+
+	public void setPropertiesFile(File propertiesFile) {
+		_propertiesFile = propertiesFile;
+	}
+
+	public void setQuiet(boolean quiet) {
+		_quiet = quiet;
+	}
+
+	@Parameter(description = "Path to Liferay bundle.", names = {"-d", "--bundleDir"}, required = true)
+	private File _bundleDir;
+
+	@Parameter(description = "Print this message.", help = true, names = {"-h", "--help"})
+	private boolean _help;
+
+	@Parameter(
+		description = "If specified, write out report to this file, otherwise uses stdout.",
+		names = {"-o", "--outputFile"}
+	)
+	private File _outputFile;
+
+	@Parameter(description = "Path to old portal-ext.properties.", names = {"-p", "--propertiesFile"}, required = true)
+	private File _propertiesFile;
+
+	@Parameter(description = "Don't write any output.", names = {"-q", "--quiet"})
+	private boolean _quiet;
+
+}
---
+++ com.liferay.blade.cli/test/com/liferay/properties/locator/PropertiesLocatorTest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.properties.locator;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.SortedSet;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ * @author Gregory Amerson
+ */
+public class PropertiesLocatorTest {
+
+	@BeforeClass
+	public static void readSystemProperties() throws Exception {
+		Assert.assertNotNull("Expecting liferay.home system property to not be null", _liferayHome);
+	}
+
+	@Test
+	public void testPropertiesLocatorAPI() throws Exception {
+		PropertiesLocatorArgs args = new PropertiesLocatorArgs();
+
+		args.setBundleDir(_liferayHome);
+		args.setPropertiesFile(new File("resources/6.2-fix-pack-131/portal.properties"));
+		args.setQuiet(true);
+
+		PropertiesLocator propertiesLocator = new PropertiesLocator(args);
+
+		SortedSet<PropertyProblem> problems = propertiesLocator.getProblems();
+
+		Assert.assertNotNull(problems);
+
+		Assert.assertEquals(problems.toString(), 627, problems.size());
+	}
+
+	@Test
+	public void testPropertiesLocatorOutputFile() throws Exception {
+		File outputFile = tempFolder.newFile("out");
+
+		String[] args = {
+			"-p", "resources/6.2-fix-pack-131/portal.properties", "-d", _liferayHome.getAbsolutePath(), "-o",
+			outputFile.getAbsolutePath()
+		};
+
+		PropertiesLocator.main(args);
+
+		String expectedOutput = new String(Files.readAllBytes(new File("test-resources/checkProperties.out").toPath()));
+		String testOutput = new String(Files.readAllBytes(outputFile.toPath()));
+
+		Assert.assertEquals(expectedOutput, testOutput);
+	}
+
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+
+	private static final File _liferayHome = new File(System.getProperty("liferay.home"));
+
+}
