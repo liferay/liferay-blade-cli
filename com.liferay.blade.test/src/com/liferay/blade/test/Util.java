95712d86451319ec1d2b4f8d8a17e8f43d3798bf
Gregory Amerson <gregory.amerson@liferay.com>
2017-09-28 14:52:08 -0500

    code has been migrated to IDE

--- com.liferay.blade.test/src/com/liferay/blade/test/Util.java
+++
@@ -1,7 +0,0 @@
-package com.liferay.blade.test;
-
-public class Util {
-	public static boolean isWindows() {
-		return System.getProperty("os.name").toLowerCase().contains("windows");
-	}
-}
--- com.liferay.blade.upgrade.liferay70/projects/filetests/PortalServiceImports.java
+++
@@ -1,6 +0,0 @@
-import com.liferay.portal.kernel.mobile.device.rulegroup.action.ActionHandler;
-import com.liferay.portal.kernel.mobile.device.rulegroup.ActionHandlerManager;
-
-public class MovedClassesImports {
-
-}
