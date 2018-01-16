79ef9915f92294b966d671957ea711a9e59b016d
Christopher Bryan Boyd <christopher.boyd@liferay.com>
2018-01-16 12:00:39 -0600

    BLADE-210: Fix Project Creation Path

--- src/main/java/com/liferay/blade/cli/CreateCommand.java
+++ src/main/java/com/liferay/blade/cli/CreateCommand.java
@@ -94,7 +94,7 @@ public class CreateCommand {
 
 		projectTemplatesArgs.setClassName(_args.getClassname());
 		projectTemplatesArgs.setContributorType(_args.getContributorType());
-		projectTemplatesArgs.setDestinationDir(dir);
+		projectTemplatesArgs.setDestinationDir(dir.getAbsoluteFile());
 		projectTemplatesArgs.setHostBundleSymbolicName(_args.getHostBundleBSN());
 		projectTemplatesArgs.setHostBundleVersion(_args.getHostBundleVersion());
 		projectTemplatesArgs.setName(name);
