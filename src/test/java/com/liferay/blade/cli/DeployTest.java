9488fb4ddb7934978c9e8d684023b48a8771f839
Gregory Amerson <gregory.amerson@liferay.com>
2018-01-26 15:06:32 -0600

    BLADE-190 update deploy test with example jar and war files

--- src/test/java/com/liferay/blade/cli/DeployTest.java
+++ src/test/java/com/liferay/blade/cli/DeployTest.java
@@ -1,114 +1,116 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.liferay.blade.cli;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import com.liferay.blade.cli.gradle.GradleExec;
+import com.liferay.blade.cli.gradle.GradleTooling;
+
 import java.io.File;
-import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 import org.easymock.EasyMock;
-import org.junit.After;
-import org.junit.Before;
+import org.easymock.IExpectationSetters;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import com.liferay.blade.cli.ShellCommandArgs;
-import com.liferay.blade.cli.gradle.GradleExec;
-import com.liferay.blade.cli.gradle.GradleTooling;
 
-import aQute.lib.io.IO;
-
+/**
+ * @author Christopher Boyd
+ */
+@PrepareForTest(
+	{GradleTooling.class, GradleExec.class, BladeCLI.class, BladeNoFail.class, Util.class, DeployCommand.class}
+)
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({GradleTooling.class, GradleExec.class, BladeCLI.class, BladeNoFail.class, Util.class, ShellCommand.class, DeployCommand.class})
 public class DeployTest {
-	private File testDir;
 
-	@Before
-	public void setUp() throws Exception {
-		testDir = Files.createTempDirectory("samplestest").toFile();
-	}
+	@Rule
+	public final TemporaryFolder tempFolder = new TemporaryFolder();
 
-	@After
-	public void cleanUp() throws Exception {
-		if (testDir.exists()) {
-			IO.delete(testDir);
-			assertFalse(testDir.exists());
-		}
-	}
-	
 	@Test
 	public void testDeployWar() throws Exception {
+		File testDir = tempFolder.newFolder();
+
 		File war = new File(testDir, "test.war");
-		
-		String fileNameWithoutExt = war.getName().substring(0,  war.getName().lastIndexOf("."));
-		
+
 		assertTrue(war.createNewFile());
 
 		PowerMock.mockStaticNice(Util.class);
-		
-		EasyMock.expect(Util.canConnect(EasyMock.anyString(), EasyMock.anyInt())).andStubReturn(true);
-		
+
+		IExpectationSetters<Boolean> canConnect = EasyMock.expect(
+			Util.canConnect(EasyMock.anyString(), EasyMock.anyInt()));
+
+		canConnect.andStubReturn(true);
+
 		PowerMock.replay(Util.class);
-		
+
 		PowerMock.mockStatic(GradleTooling.class);
-		
-		EasyMock.expect(GradleTooling.getOutputFiles(EasyMock.isA(File.class), EasyMock.isA(File.class))).andStubReturn(new HashSet<>(Arrays.asList(war)));
-		
+
+		IExpectationSetters<Set<File>> outputFiles = EasyMock.expect(
+			GradleTooling.getOutputFiles(EasyMock.isA(File.class), EasyMock.isA(File.class)));
+
+		outputFiles.andStubReturn(new HashSet<>(Arrays.asList(war)));
+
 		PowerMock.replay(GradleTooling.class);
-		
+
 		GradleExec gradle = EasyMock.createNiceMock(GradleExec.class);
-		
+
 		EasyMock.expect(gradle.executeGradleCommand(EasyMock.anyString())).andStubReturn(0);
-				
+
 		EasyMock.replay(gradle);
-		
+
 		PowerMock.mockStatic(GradleExec.class);
-		
-		PowerMock.expectNew(GradleExec.class, EasyMock.isA(BladeNoFail.class)).andStubReturn(gradle);
+
+		IExpectationSetters<GradleExec> newGradleExec = PowerMock.expectNew(
+			GradleExec.class, EasyMock.isA(BladeNoFail.class));
+
+		newGradleExec.andStubReturn(gradle);
 
 		PowerMock.replay(GradleExec.class);
-	
-		PowerMock.mockStatic(ShellCommand.class);
-		
-		PowerMock.expectNew(ShellCommand.class, EasyMock.isA(BladeNoFail.class), EasyMock.isA(ShellCommandArgs.class)).andStubAnswer(()->
-		{
-			final Object[] conArgs = EasyMock.getCurrentArguments();
-			
-			ShellCommandArgs options = (ShellCommandArgs)conArgs[1];
-			
-			ShellCommand c = EasyMock.createNiceMock(ShellCommand.class);
-			
-			c.execute();
-			
-			EasyMock.expectLastCall().andVoid().once();
-			
-			EasyMock.replay(c);
-			
-			assertEquals(options.getArgs().size(), 2);
-			
-			assertEquals(options.getArgs().get(0), "install");
-	
-			assertEquals(options.getArgs().get(1),  String.format("webbundle:%s?Web-ContextPath=/%s", war.getAbsoluteFile().toURI().toASCIIString(), fileNameWithoutExt));
-			
-			return c;
-		});
-		
-		PowerMock.replay(ShellCommand.class);
-		
-		String[] args = {
-				"-b",testDir.getAbsolutePath(), "deploy"
-		};
+
+		GogoTelnetClient client = EasyMock.createNiceMock(GogoTelnetClient.class);
+
+		PowerMock.mockStatic(GogoTelnetClient.class);
+
+		IExpectationSetters<GogoTelnetClient> newGogoTelnetClient = PowerMock.expectNew(
+			GogoTelnetClient.class, EasyMock.isA(String.class), EasyMock.isA(Integer.class));
+
+		newGogoTelnetClient.andStubReturn(client);
+
+		EasyMock.expect(client.send(EasyMock.anyString()));
+
+		String[] args = { "-b",testDir.getAbsolutePath(), "deploy" };
 
 		BladeNoFail bl= new BladeNoFail();
-		
+
 		bl.run(args);
-		
+
 		PowerMock.verifyAll();
-		
+	}
+
+	@Test
+	void testDeployJar() throws Exception {
+
 	}
 }
