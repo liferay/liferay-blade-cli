c4086da15e403bd82a239fe493d2f327e8d99f1e
christopherbryanboyd <christopher.boyd@liferay.com>
2018-06-30 14:45:30 -0500

    SF

--- cli/src/main/java/com/liferay/blade/cli/StringInputStreamWrapper.java
+++ cli/src/main/java/com/liferay/blade/cli/StringInputStreamWrapper.java
@@ -21,82 +21,26 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
 import java.nio.charset.Charset;
+
 import java.util.function.Supplier;
 
 /**
  * @author Christopher Bryan Boyd
  */
 public class StringInputStreamWrapper implements AutoCloseable, Supplier<String> {
-	
-	@Override
-	public String get() {
-		try {
-			
-			ByteArrayOutputStream result = _getOutputStream();
-			
-			return new String(result.toByteArray(), _charset);
-		} 
-		catch (Exception e) {
-			_closeSafe();
-			throw new RuntimeException(e);
-		}
 
+	public static String get(InputStream inputStream) throws Exception {
+		return get(inputStream, Charset.defaultCharset());
 	}
 
-	private void _closeSafe() {
-		try {
-			
-			close();
-		} 
-		catch (Exception ignoredException) {
-			
+	public static String get(InputStream inputStream, Charset charset) throws Exception {
+		try (StringInputStreamWrapper streamWrapper = new StringInputStreamWrapper(inputStream, charset)) {
+			return streamWrapper.get();
 		}
 	}
 
-	private static void _readInputStreamToOutputStream(InputStream inputStream, OutputStream outputStream) {
-		_readInputStreamToOutputStream(inputStream, outputStream, _DEFAULT_BUFFER_SIZE);
-	}
-	
-	private static void _readInputStreamToOutputStream(InputStream inputStream, OutputStream outputStream, int bufferSize) {
-		try {
-			byte[] buffer = new byte[bufferSize];
-			int length;
-			while ((length = inputStream.read(buffer)) != -1) {
-				outputStream.write(buffer, 0, length);
-			}
-		}
-		catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private StringInputStreamWrapper(InputStream inputStream, Charset charset) {
-
-		_inputStream = inputStream;
-		_charset = charset;
-	}
-
-	@Override
-	public String toString() {
-		return get();
-	}
-	
-	private ByteArrayOutputStream _getOutputStream() {
-		if (_outputStream == null) {
-			_outputStream = new ByteArrayOutputStream();
-			
-			_readInputStreamToOutputStream(_inputStream, _outputStream);
-		}
-		return _outputStream;
-	}
-	
-	private ByteArrayOutputStream _outputStream;
-	private Charset _charset;
-	private InputStream _inputStream;
-	private boolean _closed = false;
-	private static final int _DEFAULT_BUFFER_SIZE = 1024;
-	
 	@Override
 	public void close() throws Exception {
 		if (!_closed) {
@@ -106,36 +50,81 @@ public class StringInputStreamWrapper implements AutoCloseable, Supplier<String>
 		}
 	}
 
+	@Override
+	public String get() {
+		return _readStringFromOutputStream();
+	}
+
 	private static void _close(Closeable closeable) {
 		if (closeable != null) {
-
-			try { 
+			try {
 				closeable.close();
-			} 
-			catch (IOException ignored) {
-				
+			}
+			catch (IOException ioe) {
 			}
 		}
 	}
-	
-	public static String get(InputStream inputStream) throws Exception {
-		
-		return get(inputStream, Charset.defaultCharset());
+
+	private static void _readInputStreamToOutputStream(InputStream inputStream, OutputStream outputStream) {
+		_readInputStreamToOutputStream(inputStream, outputStream, _DEFAULT_BUFFER_SIZE);
 	}
 
-	public static String get(InputStream inputStream, Charset charset) throws Exception {
+	private static void _readInputStreamToOutputStream(
+		InputStream inputStream, OutputStream outputStream, int bufferSize) {
 
-		try (StringInputStreamWrapper streamWrapper = newInstance(inputStream, charset)) {
-			
-			return streamWrapper.get();
+		try {
+			byte[] buffer = new byte[bufferSize];
+			int length;
+			while ((length = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, length);
+			}
+		}
+		catch (IOException ioe) {
+			throw new RuntimeException(ioe);
 		}
 	}
-	
-	public static StringInputStreamWrapper newInstance(InputStream inputStream) {
-		return newInstance(inputStream, Charset.defaultCharset());
+
+	private StringInputStreamWrapper(InputStream inputStream, Charset charset) {
+		_inputStream = inputStream;
+		_charset = charset;
 	}
 
-	public static StringInputStreamWrapper newInstance(InputStream inputStream, Charset charset) {
-		return new StringInputStreamWrapper(inputStream, charset);
+	private void _closeSafe() {
+		try {
+			close();
+		}
+		catch (Exception ignoredException) {
+		}
 	}
+
+	private ByteArrayOutputStream _getOutputStream() {
+		if (_outputStream == null) {
+			_outputStream = new ByteArrayOutputStream();
+
+			_readInputStreamToOutputStream(_inputStream, _outputStream);
+		}
+
+		return _outputStream;
+	}
+
+	private String _readStringFromOutputStream() {
+		try {
+			ByteArrayOutputStream outputStream = _getOutputStream();
+
+			return new String(outputStream.toByteArray(), _charset);
+		}
+		catch (Exception e) {
+			_closeSafe();
+
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static final int _DEFAULT_BUFFER_SIZE = 1024;
+
+	private Charset _charset;
+	private boolean _closed = false;
+	private InputStream _inputStream;
+	private ByteArrayOutputStream _outputStream;
+
 }
