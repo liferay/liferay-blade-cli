167d2c0accaed69388e4c08afcd315a4d0bd78d1
Lawrence Lee <lawrence.lee@liferay.com>
2018-09-25 14:09:25 -0500

    BLADE-304 Remove default version

--- cli/src/main/java/com/liferay/blade/cli/command/CreateArgs.java
+++ cli/src/main/java/com/liferay/blade/cli/command/CreateArgs.java
@@ -129,10 +129,10 @@ public class CreateArgs extends BaseArgs {
 	private String _hostBundleVersion;
 
 	@Parameter(
-		description = "The version of Liferay to target when creating the project. Available options are 7.0, 7.1. (default 7.1).",
+		description = "The version of Liferay to target when creating the project. Available options are 7.0, 7.1.",
 		names = {"-v", "--liferay-version"}
 	)
-	private String _liferayVersion = "7.1";
+	private String _liferayVersion;
 
 	@Parameter(description = "Prints a list of available project templates", names = {"-l", "--list-templates"})
 	private boolean _listTemplates;
