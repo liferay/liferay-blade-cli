304cc531921c4c61b42ce6570d2bca1f95f7aa1e
Gregory Amerson <gregory.amerson@liferay.com>
2018-12-03 19:41:58 -0600

    BLADE-245 kill tomcat/wildfly first

--- cli/src/test/java/com/liferay/blade/cli/command/ServerStartCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/ServerStartCommandTest.java
@@ -48,6 +48,10 @@ public class ServerStartCommandTest {
 	@Before
 	public void setUp() throws Exception {
 		_testWorkspaceDir = temporaryFolder.newFolder("testWorkspaceDir");
+
+		_killTomcat();
+
+		_killWildfly();
 	}
 
 	@Test
@@ -91,34 +95,18 @@ public class ServerStartCommandTest {
 
 		Assert.assertTrue(bundlesFolder.exists());
 
-		Predicate<JavaProcess> tomcatFilter = process -> {
-			String displayName = process.getDisplayName();
-
-			return displayName.contains("org.apache.catalina.startup.Bootstrap");
-		};
-
-		Collection<JavaProcess> javaProcesses = JavaProcesses.list();
-
-		Optional<JavaProcess> tomcatProcess = _findProcess(javaProcesses, tomcatFilter);
-
-		String processList = _printDisplaNames(javaProcesses);
-
-		Assert.assertFalse(processList, tomcatProcess.isPresent());
-
 		String[] serverStartArgs = {"--base", _testWorkspaceDir.getPath(), "server", "start"};
 
 		new BladeTest().run(serverStartArgs);
 
 		Thread.sleep(1000);
 
-		javaProcesses = JavaProcesses.list();
+		Collection<JavaProcess> javaProcesses = JavaProcesses.list();
 
-		processList = _printDisplaNames(javaProcesses);
-
-		tomcatProcess = _findProcess(javaProcesses, tomcatFilter);
+		Optional<JavaProcess> tomcatProcess = _findProcess(javaProcesses, _tomcatFilter);
 
 		Assert.assertTrue(
-			"Expected tomcat process to be started.\n" + _printDisplaNames(javaProcesses), tomcatProcess.isPresent());
+			"Expected tomcat process to be started.\n" + _printDisplayNames(javaProcesses), tomcatProcess.isPresent());
 
 		JavaProcess javaProcess = tomcatProcess.get();
 
@@ -164,27 +152,15 @@ public class ServerStartCommandTest {
 
 		Assert.assertTrue(bundlesFolder.exists());
 
-		Predicate<JavaProcess> wildflyFilter = process -> {
-			String displayName = process.getDisplayName();
-
-			return displayName.contains("jboss-modules");
-		};
-
-		Collection<JavaProcess> javaProcesses = JavaProcesses.list();
-
-		Optional<JavaProcess> wildflyProcess = _findProcess(javaProcesses, wildflyFilter);
-
-		Assert.assertFalse(wildflyProcess.isPresent());
-
 		String[] serverStartArgs = {"--base", _testWorkspaceDir.getPath(), "server", "start"};
 
 		new BladeTest().run(serverStartArgs);
 
 		Thread.sleep(1000);
 
-		javaProcesses = JavaProcesses.list();
+		Collection<JavaProcess> javaProcesses = JavaProcesses.list();
 
-		wildflyProcess = _findProcess(javaProcesses, wildflyFilter);
+		Optional<JavaProcess> wildflyProcess = _findProcess(javaProcesses, _wildflyFilter);
 
 		Assert.assertTrue("Expected tomcat process to be started", wildflyProcess.isPresent());
 
@@ -241,7 +217,47 @@ public class ServerStartCommandTest {
 		).findFirst();
 	}
 
-	private String _printDisplaNames(Collection<JavaProcess> javaProcesses) {
+	private void _killTomcat() throws Exception {
+		Collection<JavaProcess> javaProcesses = JavaProcesses.list();
+
+		Optional<JavaProcess> tomcatProcess = _findProcess(javaProcesses, _tomcatFilter);
+
+		if (tomcatProcess.isPresent()) {
+			JavaProcess javaProcess = tomcatProcess.get();
+
+			PidProcess tomcatPidProcess = Processes.newPidProcess(javaProcess.getId());
+
+			Assert.assertTrue("Expected tomcat process to be alive", tomcatPidProcess.isAlive());
+
+			tomcatPidProcess.destroyForcefully();
+
+			tomcatPidProcess.waitFor(1, TimeUnit.SECONDS);
+
+			Assert.assertFalse("Expected tomcat process to be destroyed.", tomcatPidProcess.isAlive());
+		}
+	}
+
+	private void _killWildfly() throws Exception {
+		Collection<JavaProcess> javaProcesses = JavaProcesses.list();
+
+		Optional<JavaProcess> wildflyProcess = _findProcess(javaProcesses, _wildflyFilter);
+
+		if (wildflyProcess.isPresent()) {
+			JavaProcess javaProcess = wildflyProcess.get();
+
+			PidProcess wildflyPidProcess = Processes.newPidProcess(javaProcess.getId());
+
+			Assert.assertTrue("Expected wildfly process to be alive", wildflyPidProcess.isAlive());
+
+			wildflyPidProcess.destroyForcefully();
+
+			wildflyPidProcess.waitFor(1, TimeUnit.SECONDS);
+
+			Assert.assertFalse("Expected wildfly proces to be destroyed.", wildflyPidProcess.isAlive());
+		}
+	}
+
+	private String _printDisplayNames(Collection<JavaProcess> javaProcesses) {
 		StringBuilder sb = new StringBuilder();
 
 		for (JavaProcess javaProcess : javaProcesses) {
@@ -252,5 +268,17 @@ public class ServerStartCommandTest {
 	}
 
 	private File _testWorkspaceDir = null;
+
+	private Predicate<JavaProcess> _tomcatFilter = process -> {
+		String displayName = process.getDisplayName();
+
+		return displayName.contains("org.apache.catalina.startup.Bootstrap");
+	};
+
+	private Predicate<JavaProcess> _wildflyFilter = process -> {
+		String displayName = process.getDisplayName();
+
+		return displayName.contains("jboss-modules");
+	};
 
 }
