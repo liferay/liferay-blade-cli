75d5f921f1327d9fd1468497ddc4a444fd0d5806
Gregory Amerson <gregory.amerson@liferay.com>
2019-01-28 15:58:41 -0600

    BLADE-388 make snapshots testable

---
+++ publish-snapshots-test.sh
@@ -0,0 +1,14 @@
+docker stop /publish-snapshots-test
+docker rm /publish-snapshots-test
+
+docker pull sonatype/nexus:2.14.11-01 && \
+docker run -d -p 8081:8081 --name publish-snapshots-test sonatype/nexus:2.14.11-01
+
+until $(curl --output /dev/null --silent --head --fail http://localhost:8081/nexus/); do
+  printf '.'
+  sleep 5
+done
+
+./gradlew clean && \
+./gradlew -PlocalNexus -Psnapshots :extensions:maven-profile:publish && \
+./gradlew -PlocalNexus -Psnapshots --refresh-dependencies :cli:publish
--- publish-snapshots.sh
+++ publish-snapshots.sh
@@ -1,3 +1,3 @@
-./gradlew clean
-./gradlew -Psnapshots :extensions:maven-profile:publish
+./gradlew clean && \
+./gradlew -Psnapshots :extensions:maven-profile:publish && \
 ./gradlew -Psnapshots --refresh-dependencies :cli:publish
