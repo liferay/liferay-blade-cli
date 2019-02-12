633dc874610d5c5662efb43fba3183a90ae85e00
Christopher Bryan Boyd <christopher.boyd@liferay.com>
2019-02-11 23:40:52 -0600

    BLADE-395 -p, --port, -s, --suspend features added for blade server start and run

--- cli/src/main/java/com/liferay/blade/cli/command/ServerStartArgs.java
+++ cli/src/main/java/com/liferay/blade/cli/command/ServerStartArgs.java
@@ -20,14 +20,23 @@ import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 
 /**
+ * @author Christopher Bryan Boyd
  * @author Gregory Amerson
  * @author Simon Jiang
  */
 @Parameters(commandDescription = "Start server defined by your Liferay project", commandNames = "server start")
 public class ServerStartArgs extends BaseArgs {
 
+	public int getDebugPort() {
+		return _debugPort;
+	}
+
 	public boolean isDebug() {
 		return _debug;
+	}
+
+	public boolean isSuspend() {
+		return _suspend;
 	}
 
 	public boolean isTail() {
@@ -40,6 +49,15 @@ public class ServerStartArgs extends BaseArgs {
 
 	@Parameter(description = "Start server in debug mode", names = {"-d", "--debug"})
 	private boolean _debug;
+
+	@Parameter(description = "Debug port number in debug mode", names = {"-p", "--port"})
+	private int _debugPort = -1;
+
+	@Parameter(
+		description = "When in debug mode, suspend the started server until the debugger is connected",
+		names = {"-s", "--suspend"}
+	)
+	private boolean _suspend = false;
 
 	@Parameter(description = "Tail the started server", names = {"-t", "--tail"})
 	private boolean _tail;
