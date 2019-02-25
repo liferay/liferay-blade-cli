3b7f382d178cd73010f62641ea99b1e53c84b722
Gregory Amerson <gregory.amerson@liferay.com>
2019-02-25 17:32:42 -0600

    use real temp folder

--- publish-test.sh
+++ publish-test.sh
@@ -1,4 +1,3 @@
-
 if [ "${1}" != "release" -a "${1}" != "snapshots" ]; then
 	echo "Must have one argument, either \"release\" or \"snapshots\"."
 	exit 1
@@ -16,47 +15,55 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:8081/nex
 done
 
 timestamp=$(date +%s)
-mkdir -p tmp/$timestamp/cli
+mkdir -p /tmp/$timestamp/cli
 
 ./gradlew clean
+
 if [ "$?" != "0" ]; then
-	echo Houston, we have a problem. failed clean
+	echo Failed clean.
+	rm -rf /tmp/$timestamp
 	exit 1
 fi
 
-
 mavenProfileUrl=`./gradlew -PlocalNexus -P${1} :extensions:maven-profile:publish --info | grep Uploading | grep '.jar ' | grep -v -e '-sources' -e '-tests' | cut -d' ' -f2`
+
 if [ "$?" != "0" ]; then
-	echo Houston, we have a problem. failed :extensions:maven-profile:publish info
+	echo Failed :extensions:maven-profile:publish
+	rm -rf /tmp/$timestamp
 	exit 1
 fi
 
 mavenProfileUrl="http://localhost:8081/nexus/content/groups/public/"$mavenProfileUrl
 
-curl -s $mavenProfileUrl -o tmp/$timestamp/maven_profile.jar
+curl -s $mavenProfileUrl -o /tmp/$timestamp/maven_profile.jar
 
 ./gradlew -PlocalNexus -P${1} --refresh-dependencies --scan clean check :cli:smokeTests
+
 if [ "$?" != "0" ]; then
-	echo Houston, we have a problem. failed check and smokeTests.
+	echo Failed check and smokeTests.
+	rm -rf /tmp/$timestamp
 	exit 1
 fi
 
 bladeCliUrl=`./gradlew -PlocalNexus -P${1} :cli:publish --info | grep Uploading | grep '.jar ' | grep -v -e '-sources' -e '-tests' | cut -d' ' -f2`
+
 if [ "$?" != "0" ]; then
-	echo Houston, we have a problem. failed :cli:publish info
+	echo Failed :cli:publish
+	rm -rf /tmp/$timestamp
 	exit 1
 fi
 
 bladeCliUrl="http://localhost:8081/nexus/content/groups/public/"$bladeCliUrl
 
-curl -s $bladeCliUrl -o tmp/$timestamp/cli/blade.jar
+curl -s $bladeCliUrl -o /tmp/$timestamp/cli/blade.jar
 
-mavenProfileJar=`jar -tf tmp/$timestamp/cli/blade.jar | grep "maven.profile-"`
+mavenProfileJar=`jar -tf /tmp/$timestamp/cli/blade.jar | grep "maven.profile-"`
 
-unzip -p tmp/$timestamp/cli/blade.jar $mavenProfileJar > tmp/$timestamp/cli/myExtractedMavenProfile.jar
+unzip -p /tmp/$timestamp/cli/blade.jar $mavenProfileJar > /tmp/$timestamp/cli/myExtractedMavenProfile.jar
 
 echo $mavenProfileUrl
 echo $bladeCliUrl
-diff -s tmp/$timestamp/cli/myExtractedMavenProfile.jar tmp/$timestamp/maven_profile.jar
 
-rm -rf tmp/
+diff -s /tmp/$timestamp/cli/myExtractedMavenProfile.jar /tmp/$timestamp/maven_profile.jar
+
+rm -rf /tmp/$timestamp
