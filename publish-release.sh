dae6dffde28c20d3420d6bb34c8bc6e661a68c33
Gregory Amerson <gregory.amerson@liferay.com>
2019-02-25 19:04:31 -0600

    temporarily remove maven profile publish

--- publish-release.sh
+++ publish-release.sh
@@ -1,3 +1,2 @@
 ./gradlew --no-daemon clean && \
-./gradlew --no-daemon -Prelease :extensions:maven-profile:publish --info --scan && \
 ./gradlew --no-daemon -Prelease --refresh-dependencies clean :cli:publish --info --scan
