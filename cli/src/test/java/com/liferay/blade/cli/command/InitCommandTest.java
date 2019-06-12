d182be2aa80b0fd2985713a25e9976c0655178f0
Lawrence Lee <lawrence.lee@liferay.com>
2019-06-12 10:46:15 -0500

    BLADE-441 Upgraded to workspace 2.0.4

--- cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
@@ -581,7 +581,7 @@ public class InitCommandTest {
 		GradleRunnerUtil.verifyBuildOutput(projectPath.toString(), "foo-1.0.0.jar");
 	}
 
-	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "2.0.3";
+	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "2.0.4";
 
 	private File _extensionsDir = null;
 	private File _workspaceDir = null;
