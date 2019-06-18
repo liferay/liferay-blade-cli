c1ebbd39a6fdad0814f86d89de50763e7f031e83
Christopher Bryan Boyd <christopher.boyd@liferay.com>
2019-06-18 16:18:11 -0500

    BLADE-452 SF

--- extensions/maven-profile/src/main/java/com/liferay/blade/extensions/maven/profile/internal/MavenExecutor.java
+++ extensions/maven-profile/src/main/java/com/liferay/blade/extensions/maven/profile/internal/MavenExecutor.java
@@ -40,11 +40,10 @@ public interface MavenExecutor {
 		if (!(args.length > 0)) {
 			throw new RuntimeException("Args must be specified");
 		}
-		
+
 		File projectFile = new File(projectPath);
-		
+
 		try {
-		
 			projectPath = projectFile.getCanonicalPath();
 		}
 		catch (Exception e) {
