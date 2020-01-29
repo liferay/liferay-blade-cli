b75dc4970eaa49994a55b287e67a84c7487a933a
Gregory Amerson <gregory.amerson@liferay.com>
2020-01-29 08:53:12 -0600

    BLADE-502 test scripts update

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,23 @@
-rm -rf ~/.m2/repository/com/liferay/blade
-./gradlew --no-daemon clean
+#!/bin/bash
+
+function archiveTests {
+	find . -name index.html | grep build/reports/tests | sed 's/\/test\/index\.html//' | xargs zip -o tests.zip -r
+}
+
+function checkError {
+	if [ $? -ne 0 ]; then
+		local ERRCODE = $?
+
+		archiveTests
+
+		exit $ERRCODE
+	fi
+}
+
 ./gradlew --no-daemon :extensions:maven-profile:publishToMavenLocal -x :cli:bladeExtensionsVersions -x :cli:processResources --scan
+
+checkError
+
 ./gradlew --no-daemon -PmavenLocal --continue clean check smokeTests --scan
-find . -name index.html | grep build/reports/tests | sed 's/\/test\/index\.html//' | xargs zip -o tests.zip -r
+
+checkError
