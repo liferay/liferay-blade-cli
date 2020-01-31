cbe46d1073e3d9839b96eb38b3e7f8b3409e26c1
Gregory Amerson <gregory.amerson@liferay.com>
2020-01-31 16:49:47 -0600

    BLADE-501 BLADE-504 Don't print error stacktrace for gradle executions we just let original error be seen

--- cli/src/main/java/com/liferay/blade/cli/BladeCLI.java
+++ cli/src/main/java/com/liferay/blade/cli/BladeCLI.java
@@ -124,7 +124,6 @@ public class BladeCLI {
 			bladeCLI.run(args);
 		}
 		catch (GradleExecutionException e) {
-			bladeCLI.error(e);
 			System.exit(e.getReturnCode());
 		}
 		catch (Exception e) {
