3d02b9883412a8a405b3aff8b4acf8dcd851e234
Lawrence Lee <lawrence.lee@liferay.com>
2020-08-03 14:17:30 -0500

    LPS-105873 Update Test

--- cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
@@ -647,7 +647,7 @@ public class InitCommandTest {
 		GradleRunnerUtil.verifyBuildOutput(projectPath.toString(), "foo-1.0.0.jar");
 	}
 
-	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "2.5.3";
+	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "3.0.3";
 
 	private File _extensionsDir = null;
 	private File _workspaceDir = null;
