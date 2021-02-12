3dccf1fae486efdd377881cdc9055afef2ff6380
Lawrence Lee <lawrence.lee@liferay.com>
2021-02-12 09:29:20 -0600

    IDE-4932 Update gradle-plugins-theme-builder with correct gradle-plugins-css-builder dependency for gradle 5.6.4 compatibility

--- cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
@@ -640,7 +640,7 @@ public class InitCommandTest {
 		GradleRunnerUtil.verifyBuildOutput(projectPath.toString(), "foo-1.0.0.jar");
 	}
 
-	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "3.4.1";
+	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "3.4.2";
 
 	private File _extensionsDir = null;
 	private File _workspaceDir = null;
