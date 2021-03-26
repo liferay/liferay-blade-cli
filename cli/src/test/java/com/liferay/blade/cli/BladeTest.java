8bd20a20faec7bf5cffac1314d63d02c6a8594c1
Lawrence Lee <lawrence.lee@liferay.com>
2021-03-26 11:45:33 -0500

    BLADE-629 Use latest 7.3 GA

--- cli/src/test/java/com/liferay/blade/cli/BladeTest.java
+++ cli/src/test/java/com/liferay/blade/cli/BladeTest.java
@@ -64,7 +64,7 @@ public class BladeTest extends BladeCLI {
 
 	public static final String PRODUCT_VERSION_PORTAL_72 = "portal-7.2-ga2";
 
-	public static final String PRODUCT_VERSION_PORTAL_73 = "portal-7.3-ga6";
+	public static final String PRODUCT_VERSION_PORTAL_73 = "portal-7.3-ga7";
 
 	public static final String PRODUCT_VERSION_PORTAL_74 = "portal-7.4-ga1";
 
