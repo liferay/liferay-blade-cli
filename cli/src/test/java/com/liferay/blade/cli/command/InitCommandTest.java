f0cc02076eedae6ad66460f7fb8134aaf778928d
Lawrence Lee <lawrence.lee@liferay.com>
2021-05-04 12:17:13 -0500

    LPS-130908 Update workspace version for 7.4

--- cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
@@ -651,7 +651,7 @@ public class InitCommandTest {
 		GradleRunnerUtil.verifyBuildOutput(projectPath.toString(), "foo-1.0.0.jar");
 	}
 
-	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "3.4.8";
+	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "3.4.9";
 
 	private File _extensionsDir = null;
 	private File _workspaceDir = null;
