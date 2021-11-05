c583ccd5365b0fba5621c91288c78976b90fa560
Ashley Yuan <yanan.yuan@liferay.com>
2021-11-05 13:00:50 +0800

    LPS-141989 fix tests

--- cli/src/test/java/com/liferay/blade/cli/BladeTest.java
+++ cli/src/test/java/com/liferay/blade/cli/BladeTest.java
@@ -66,7 +66,7 @@ public class BladeTest extends BladeCLI {
 
 	public static final String PRODUCT_VERSION_PORTAL_73 = "portal-7.3-ga8";
 
-	public static final String PRODUCT_VERSION_PORTAL_74 = "portal-7.4-ga2";
+	public static final String PRODUCT_VERSION_PORTAL_74 = "portal-7.4-ga4";
 
 	public static BladeTestBuilder builder() {
 		return new BladeTestBuilder();
