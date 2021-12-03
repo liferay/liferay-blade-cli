f22b8eac5306a455efd4f6aadd5cd68920852aed
Ashley Yuan <yanan.yuan@liferay.com>
2021-12-03 11:40:26 +0800

    BLADE-644 update to latest theme builder plugin

--- cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
@@ -657,7 +657,7 @@ public class InitCommandTest {
 		GradleRunnerUtil.verifyBuildOutput(projectPath.toString(), "foo-1.0.0.jar");
 	}
 
-	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "3.4.17";
+	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "3.4.19";
 
 	private File _extensionsDir = null;
 	private File _workspaceDir = null;
