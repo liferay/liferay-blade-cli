636239cae1ac71e7de51c9886ea22527f0ce8731
Simon.Jiang <simon.jiang@liferay.com>
2022-05-18 11:50:35 +0800

    BLADE-678 use latest project template

--- cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
@@ -657,7 +657,7 @@ public class InitCommandTest {
 		GradleRunnerUtil.verifyBuildOutput(projectPath.toString(), "foo-1.0.0.jar");
 	}
 
-	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "3.4.27";
+	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "3.4.38";
 
 	private File _extensionsDir = null;
 	private File _workspaceDir = null;
