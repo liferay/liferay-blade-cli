1917c51daa864230895a0225da977b93fe5bf0d9
Gregory Amerson <gregory.amerson@liferay.com>
2022-06-22 12:59:32 -0500

    LCD-14295 update test version check

--- cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
@@ -653,7 +653,7 @@ public class InitCommandTest {
 		GradleRunnerUtil.verifyBuildOutput(projectPath.toString(), "foo-1.0.0.jar");
 	}
 
-	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "3.4.38";
+	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "4.0.0";
 
 	private File _extensionsDir = null;
 	private File _workspaceDir = null;
