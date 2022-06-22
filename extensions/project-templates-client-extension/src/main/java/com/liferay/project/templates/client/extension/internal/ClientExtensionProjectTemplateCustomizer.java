357376458b1eb1abb5185538785ff55806dd4447
Gregory Amerson <gregory.amerson@liferay.com>
2022-06-21 21:58:32 -0500

    LCD-14267 add a new project-templates-client-extension

---
+++ extensions/project-templates-client-extension/src/main/java/com/liferay/project/templates/client/extension/internal/ClientExtensionProjectTemplateCustomizer.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liferay.project.templates.client.extension.internal;
+
+import com.liferay.project.templates.extensions.ProjectTemplateCustomizer;
+import com.liferay.project.templates.extensions.ProjectTemplatesArgs;
+
+import java.io.File;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.archetype.ArchetypeGenerationRequest;
+import org.apache.maven.archetype.ArchetypeGenerationResult;
+
+/**
+ * @author Gregory Amerson
+ */
+public class ClientExtensionProjectTemplateCustomizer implements ProjectTemplateCustomizer {
+
+	@Override
+	public String getTemplateName() {
+		return "client-extension";
+	}
+
+	@Override
+	public void onAfterGenerateProject(
+			ProjectTemplatesArgs projectTemplatesArgs, File destinationDir,
+			ArchetypeGenerationResult archetypeGenerationResult)
+		throws Exception {
+	}
+
+	@Override
+	public void onBeforeGenerateProject(
+			ProjectTemplatesArgs projectTemplatesArgs, ArchetypeGenerationRequest archetypeGenerationRequest)
+		throws Exception {
+
+		File destinationDir = projectTemplatesArgs.getDestinationDir();
+
+		String projectName = projectTemplatesArgs.getName();
+
+		ClientExtensionProjectTemplatesArgsExt clientExtensionTemplateExt =
+			(ClientExtensionProjectTemplatesArgsExt)projectTemplatesArgs.getProjectTemplatesArgsExt();
+
+		List<String> args = new ArrayList<>();
+
+		args.add("generate");
+		args.add("-i");
+		args.add(projectName);
+
+		String extensionName = clientExtensionTemplateExt.getExtensionName();
+
+		if (extensionName != null) {
+			args.add("-n");
+			args.add(extensionName);
+		}
+
+		String extensionType = clientExtensionTemplateExt.getExtensionType();
+
+		if (extensionType != null) {
+			args.add("-t");
+			args.add(extensionType);
+		}
+
+		LXCUtil.run(destinationDir.toPath(), args.toArray(new String[0]), false);
+	}
+
+}
---
+++ extensions/project-templates-client-extension/src/test/java/com/liferay/project/templates/client/extension/ClientExtensionProjectTemplateTest.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liferay.project.templates.client.extension;
+
+import com.liferay.blade.cli.BladeTest;
+import com.liferay.blade.cli.TestUtil;
+import com.liferay.portal.tools.bundle.support.internal.util.FileUtil;
+
+import java.io.File;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ * @author Gregory Amerson
+ */
+public class ClientExtensionProjectTemplateTest {
+
+	@Before
+	public void setUp() throws Exception {
+		File extensionsDir = _getExtensionsDir();
+
+		_extensionsDirPath = extensionsDir.toPath();
+
+		File settingsDir = temporaryFolder.getRoot();
+
+		Path settingsDirPath = settingsDir.toPath();
+
+		BladeTest.BladeTestBuilder bladeTestBuilder = BladeTest.builder();
+
+		bladeTestBuilder.setSettingsDir(settingsDirPath);
+
+		bladeTestBuilder.setExtensionsDir(_extensionsDirPath);
+
+		_bladeTest = bladeTestBuilder.build();
+
+		_rootDir = temporaryFolder.getRoot();
+	}
+
+	@Test
+	public void testClientExtensionCustomElementProjectTemplate() throws Exception {
+		_setupTestExtensions();
+
+		File workspaceDir = new File(_rootDir, "workspace");
+
+		_makeWorkspace(workspaceDir);
+
+		String[] args = {
+			"create", "--base", workspaceDir.getAbsolutePath(), "-t", "client-extension", "-d",
+			workspaceDir.getAbsolutePath(), "--extension-name", "test123", "--extension-type", "customElement",
+			"customelementtest"
+		};
+
+		_bladeTest.run(args);
+
+		File projectDir = new File(workspaceDir, "customelementtest");
+
+		Assert.assertTrue("Expected project dir to exist " + projectDir, projectDir.exists());
+
+		File clientExtensionFile = new File(projectDir, "client-extension.yaml");
+
+		Assert.assertTrue(
+			"Expected client-extension.yaml file to exist " + clientExtensionFile, clientExtensionFile.exists());
+
+		String content = FileUtil.read(clientExtensionFile);
+
+		Assert.assertTrue("Expected client-extension.yaml to contain test123\n" + content, content.contains("test123"));
+	}
+
+	@Rule
+	public final TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+	private File _getExtensionsDir() {
+		return new File(temporaryFolder.getRoot(), ".blade/extensions");
+	}
+
+	private void _makeWorkspace(File workspace) throws Exception {
+		File parentFile = workspace.getParentFile();
+
+		String[] args = {
+			"--base", parentFile.getPath(), "init", workspace.getName(), "-v", BladeTest.PRODUCT_VERSION_DXP_74
+		};
+
+		TestUtil.runBlade(workspace, _extensionsDirPath.toFile(), args);
+	}
+
+	private void _setupTestExtension(Path extensionsPath, String jarPath) throws Exception {
+		File sampleJarFile = new File(jarPath);
+
+		Assert.assertTrue(sampleJarFile.getAbsolutePath() + " does not exist.", sampleJarFile.exists());
+
+		Path sampleJarPath = extensionsPath.resolve(sampleJarFile.getName());
+
+		Files.copy(sampleJarFile.toPath(), sampleJarPath, StandardCopyOption.REPLACE_EXISTING);
+
+		Assert.assertTrue(Files.exists(sampleJarPath));
+	}
+
+	private void _setupTestExtensions() throws Exception {
+		File extensionsDir = _getExtensionsDir();
+
+		extensionsDir.mkdirs();
+
+		Assert.assertTrue("Unable to create test extensions dir.", extensionsDir.exists());
+
+		Path extensionsPath = extensionsDir.toPath();
+
+		_setupTestExtension(extensionsPath, System.getProperty("clientExtensionTemplateJarFile"));
+	}
+
+	private BladeTest _bladeTest;
+	private Path _extensionsDirPath;
+	private File _rootDir;
+
+}
