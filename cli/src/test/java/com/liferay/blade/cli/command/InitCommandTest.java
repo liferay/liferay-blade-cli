1653063764dde21cf5889d093fc0e5bb3791a734
drewbrokke <drew.brokke@liferay.com>
2023-08-17 19:01:41 -0500

    BLADE-724 cli: bumps expected workspace version

--- cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
@@ -679,7 +679,7 @@ public class InitCommandTest {
 		GradleRunnerUtil.verifyBuildOutput(projectPath.toString(), "foo-1.0.0.jar");
 	}
 
-	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "6.1.0";
+	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "8.0.3";
 
 	private File _extensionsDir = null;
 	private File _workspaceDir = null;
