eb067640982d86a91cac75cb78dfdc3aaf0e90b8
drewbrokke <drew.brokke@liferay.com>
2024-03-29 14:07:58 -0500

    BLADE-743 cli: CreateCommand: provides exception for quarterly versions

--- cli/src/main/java/com/liferay/blade/cli/command/CreateCommand.java
+++ cli/src/main/java/com/liferay/blade/cli/command/CreateCommand.java
@@ -472,6 +472,12 @@ public class CreateCommand extends BaseCommand<CreateArgs> {
 	}
 
 	private String _checkTemplateVersionRange(File templateFile, ProjectTemplatesArgs projectTemplatesArgs) {
+		String versionString = projectTemplatesArgs.getLiferayVersion();
+
+		if (VersionUtil.isLiferayQuarterlyVersion(versionString)) {
+			return "";
+		}
+
 		try (InputStream fileInputStream = Files.newInputStream(templateFile.toPath(), StandardOpenOption.READ);
 			JarInputStream in = new JarInputStream(fileInputStream)) {
 
@@ -483,17 +489,14 @@ public class CreateCommand extends BaseCommand<CreateArgs> {
 
 			VersionRange versionRange = new VersionRange(versionRangeValue);
 
-			String versionString = projectTemplatesArgs.getLiferayVersion();
-
-			String liferayVersionString = new String(
-				String.valueOf(VersionUtil.getMajorVersion(versionString)) + "." +
-					String.valueOf(VersionUtil.getMinorVersion(versionString)));
+			String liferayVersionString = String.format(
+				"%s.%s", VersionUtil.getMajorVersion(versionString), VersionUtil.getMinorVersion(versionString));
 
 			if (!versionRange.includes(Version.parseVersion(liferayVersionString))) {
-				return new String(
-					"Error: The " + projectTemplatesArgs.getTemplate() +
-						" project can only be created in liferay version range: " + versionRange +
-							", current liferay version is " + liferayVersionString + ".");
+				return String.format(
+					"Error: The %s project can only be created in liferay version range: %s, current liferay version " +
+						"is %s.",
+					projectTemplatesArgs.getTemplate(), versionRange, liferayVersionString);
 			}
 		}
 		catch (Exception exception) {
