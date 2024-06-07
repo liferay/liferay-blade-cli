77df6997336217cf8f20fc941122de7a85d73374
drewbrokke <drew.brokke@liferay.com>
2024-06-07 14:48:02 -0500

    LPD-28093 cli: test: updates expected workspace version

--- cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
@@ -621,7 +621,7 @@ public class InitCommandTest {
 		GradleRunnerUtil.verifyBuildOutput(projectPath.toString(), "foo-1.0.0.jar");
 	}
 
-	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "10.0.3";
+	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "10.1.0";
 
 	private File _extensionsDir = null;
 	private File _workspaceDir = null;
