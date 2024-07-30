3d8c2ebc818992763272d4abae87625811ef2cf2
drewbrokke <drew.brokke@liferay.com>
2024-07-30 16:48:30 -0500

    LPD-32781 liferay-blade-cli: ReleaseUtil extends from lib ReleaseUtil

--- cli/src/main/java/com/liferay/blade/cli/util/ReleaseUtil.java
+++ cli/src/main/java/com/liferay/blade/cli/util/ReleaseUtil.java
@@ -5,140 +5,54 @@
 
 package com.liferay.blade.cli.util;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.io.File;
-
-import java.time.temporal.ChronoUnit;
+import com.liferay.release.util.ReleaseEntry;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 /**
  * @author Drew Brokke
  */
-public class ReleaseUtil {
+public class ReleaseUtil extends com.liferay.release.util.ReleaseUtil {
 
-	public static ReleaseEntry getReleaseEntry(String releaseKey) {
-		if (_releaseUtil == null) {
-			populateReleases(_DEFAULT_MAX_AGE);
-		}
-
-		return _releaseUtil._releaseEntryMap.getOrDefault(releaseKey, _EMPTY_RELEASE_ENTRY);
+	public static ReleaseEntry getReleaseEntry(String version) {
+		return getReleaseEntry(null, version);
 	}
 
-	public static void populateReleases(int maxAge) {
-		_releaseUtil = new ReleaseUtil(maxAge);
-	}
+	public static ReleaseEntry getReleaseEntry(String product, String version) {
+		Predicate<ReleaseEntry> productGroupVersionPredicate = releaseEntry -> Objects.equals(
+			releaseEntry.getProductGroupVersion(), version);
+		Predicate<ReleaseEntry> productPredicate = releaseEntry -> Objects.equals(releaseEntry.getProduct(), product);
+		Predicate<ReleaseEntry> targetPlatformVersionPredicate = releaseEntry -> Objects.equals(
+			releaseEntry.getTargetPlatformVersion(), version);
 
-	public static Stream<ReleaseEntry> releaseEntriesStream() {
-		if (_releaseUtil == null) {
-			populateReleases(_DEFAULT_MAX_AGE);
+		List<Predicate<ReleaseEntry>> predicates = new ArrayList<>();
+
+		predicates.add(releaseEntry -> Objects.equals(releaseEntry.getReleaseKey(), version));
+
+		predicates.add(productPredicate.and(targetPlatformVersionPredicate));
+
+		predicates.add(productPredicate.and(productGroupVersionPredicate));
+
+		predicates.add(targetPlatformVersionPredicate);
+
+		predicates.add(productGroupVersionPredicate);
+
+		for (Predicate<ReleaseEntry> predicate : predicates) {
+			Optional<ReleaseEntry> first = getReleaseEntryStream(
+			).filter(
+				predicate
+			).findFirst();
+
+			if (first.isPresent()) {
+				return first.get();
+			}
 		}
 
-		return _releaseUtil._releaseEntries.stream();
-	}
-
-	public static <T> T withReleaseEntriesStream(Function<Stream<ReleaseEntry>, T> function) {
-		return function.apply(releaseEntriesStream());
-	}
-
-	public static <T> T withReleaseEntry(String releaseKey, Function<ReleaseEntry, T> function) {
-		return function.apply(getReleaseEntry(releaseKey));
-	}
-
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static class ReleaseEntry {
-
-		public String getProduct() {
-			return _product;
-		}
-
-		public String getProductGroupVersion() {
-			return _productGroupVersion;
-		}
-
-		public String getProductVersion() {
-			return _productVersion;
-		}
-
-		public String getReleaseKey() {
-			return _releaseKey;
-		}
-
-		public String getTargetPlatformVersion() {
-			return _targetPlatformVersion;
-		}
-
-		public String getUrl() {
-			return _url;
-		}
-
-		public boolean isPromoted() {
-			return _promoted;
-		}
-
-		@JsonProperty("product")
-		private String _product;
-
-		@JsonProperty("productGroupVersion")
-		private String _productGroupVersion;
-
-		@JsonProperty("productVersion")
-		private String _productVersion;
-
-		@JsonProperty("promoted")
-		private boolean _promoted;
-
-		@JsonProperty("releaseKey")
-		private String _releaseKey;
-
-		@JsonProperty("targetPlatformVersion")
-		private String _targetPlatformVersion;
-
-		@JsonProperty("url")
-		private String _url;
-
-	}
-
-	private ReleaseUtil(int maxAge) {
-		File releasesJsonFile = new File(_workspaceCacheDir, "releases.json");
-
-		_releaseEntries = ResourceUtil.readJson(
-			ReleaseEntries.class, ResourceUtil.getLocalFileResolver(System.getenv("BLADE_LOCAL_RELEASES_JSON_FILE")),
-			ResourceUtil.getLocalFileResolver(releasesJsonFile, maxAge, ChronoUnit.DAYS),
-			ResourceUtil.getURLResolver(
-				_workspaceCacheDir, "https://releases-cdn.liferay.com/releases.json", "releases.json"),
-			ResourceUtil.getURLResolver(
-				_workspaceCacheDir, "https://releases.liferay.com/releases.json", "releases.json"),
-			ResourceUtil.getClassLoaderResolver("/releases.json"));
-
-		if (_releaseEntries == null) {
-			throw new RuntimeException("Could not find releases.json");
-		}
-
-		_releaseEntryMap.clear();
-
-		for (ReleaseEntry releaseEntry : _releaseEntries) {
-			_releaseEntryMap.put(releaseEntry.getReleaseKey(), releaseEntry);
-		}
-	}
-
-	private static final int _DEFAULT_MAX_AGE = 7;
-
-	private static final ReleaseEntry _EMPTY_RELEASE_ENTRY = new ReleaseEntry();
-
-	private static ReleaseUtil _releaseUtil;
-
-	private final ReleaseEntries _releaseEntries;
-	private final Map<String, ReleaseEntry> _releaseEntryMap = new HashMap<>();
-	private final File _workspaceCacheDir = new File(System.getProperty("user.home"), ".liferay/workspace");
-
-	private static class ReleaseEntries extends ArrayList<ReleaseEntry> {
+		return null;
 	}
 
 }
