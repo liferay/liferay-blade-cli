450df4bed255508594761243e8a9fcf28ffb919b
drewbrokke <drew.brokke@liferay.com>
2024-09-30 12:38:18 -0500

    LPD-37083 cli: bumps expected version of workspace in tests to 12.0.0

--- cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
@@ -618,7 +618,7 @@ public class InitCommandTest {
 		GradleRunnerUtil.verifyBuildOutput(projectPath.toString(), "foo-1.0.0.jar");
 	}
 
-	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "10.1.7";
+	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "12.0.0";
 
 	private File _extensionsDir = null;
 	private File _workspaceDir = null;
